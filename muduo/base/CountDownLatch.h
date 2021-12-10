7bc38e85c407fe6a62ea8b4b1f6afaede0607c3b
jie.jiang <jiecfd@gmail.com>
2021-12-10 17:17:40 +0800

    latch注释

--- muduo/base/CountDownLatch.h
+++ muduo/base/CountDownLatch.h
@@ -2,6 +2,8 @@
 // that can be found in the License file.
 //
 // Author: Shuo Chen (chenshuo at chenshuo dot com)
+// 既可以用于所有子线程等待主线程发起 "起跑"
+// 也可以用于主线程等待子线程初始化完毕才开始工作
 
 #ifndef MUDUO_BASE_COUNTDOWNLATCH_H
 #define MUDUO_BASE_COUNTDOWNLATCH_H
