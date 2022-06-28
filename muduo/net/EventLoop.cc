153cc62e642bcfc6e3ed0da6ca123bcac4e34db8
lawliet <jiecfd@gmail.com>
2022-06-28 14:01:42 +0800

    eventloop格式化

--- muduo/net/EventLoop.cc
+++ muduo/net/EventLoop.cc
@@ -8,34 +8,31 @@
 
 #include "muduo/net/EventLoop.h"
 
+#include <signal.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+
+#include <algorithm>
+
 #include "muduo/base/Logging.h"
 #include "muduo/net/Channel.h"
 #include "muduo/net/Poller.h"
 #include "muduo/net/SocketsOps.h"
 #include "muduo/net/TimerQueue.h"
 
-#include <algorithm>
-
-#include <signal.h>
-#include <sys/eventfd.h>
-#include <unistd.h>
-
 using namespace muduo;
 using namespace muduo::net;
 
-namespace
-{
+namespace {
 // 当前线程EventLoop对象指针
 // 线程局部存储
 __thread EventLoop* t_loopInThisThread = 0;
 
 const int kPollTimeMs = 10000;
 
-int createEventfd()
-{
+int createEventfd() {
   int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
-  if (evtfd < 0)
-  {
+  if (evtfd < 0) {
     LOG_SYSERR << "Failed in eventfd";
     abort();
   }
@@ -43,11 +40,9 @@ int createEventfd()
 }
 
 #pragma GCC diagnostic ignored "-Wold-style-cast"
-class IgnoreSigPipe
-{
+class IgnoreSigPipe {
  public:
-  IgnoreSigPipe()
-  {
+  IgnoreSigPipe() {
     ::signal(SIGPIPE, SIG_IGN);
     // LOG_TRACE << "Ignore SIGPIPE";
   }
@@ -57,42 +52,35 @@ class IgnoreSigPipe
 IgnoreSigPipe initObj;
 }  // namespace
 
-EventLoop* EventLoop::getEventLoopOfCurrentThread()
-{
+EventLoop* EventLoop::getEventLoopOfCurrentThread() {
   return t_loopInThisThread;
 }
 
 EventLoop::EventLoop()
-  : looping_(false),
-    quit_(false),
-    eventHandling_(false),
-    callingPendingFunctors_(false),
-    iteration_(0),
-    threadId_(CurrentThread::tid()),
-    poller_(Poller::newDefaultPoller(this)),
-    timerQueue_(new TimerQueue(this)),
-    wakeupFd_(createEventfd()),
-    wakeupChannel_(new Channel(this, wakeupFd_)),
-    currentActiveChannel_(NULL)
-{
+    : looping_(false),
+      quit_(false),
+      eventHandling_(false),
+      callingPendingFunctors_(false),
+      iteration_(0),
+      threadId_(CurrentThread::tid()),
+      poller_(Poller::newDefaultPoller(this)),
+      timerQueue_(new TimerQueue(this)),
+      wakeupFd_(createEventfd()),
+      wakeupChannel_(new Channel(this, wakeupFd_)),
+      currentActiveChannel_(NULL) {
   LOG_DEBUG << "EventLoop created " << this << " in thread " << threadId_;
-  if (t_loopInThisThread)
-  {
+  if (t_loopInThisThread) {
     LOG_FATAL << "Another EventLoop " << t_loopInThisThread
               << " exists in this thread " << threadId_;
-  }
-  else
-  {
+  } else {
     t_loopInThisThread = this;
   }
-  wakeupChannel_->setReadCallback(
-      std::bind(&EventLoop::handleRead, this));
+  wakeupChannel_->setReadCallback(std::bind(&EventLoop::handleRead, this));
   // we are always reading the wakeupfd
   wakeupChannel_->enableReading();
 }
 
-EventLoop::~EventLoop()
-{
+EventLoop::~EventLoop() {
   LOG_DEBUG << "EventLoop " << this << " of thread " << threadId_
             << " destructs in thread " << CurrentThread::tid();
   wakeupChannel_->disableAll();
@@ -103,8 +91,7 @@ EventLoop::~EventLoop()
 
 // 事件循环, 该函数不能跨线程调用
 // 只能在创建该对象的线程中调用
-void EventLoop::loop()
-{
+void EventLoop::loop() {
   assert(!looping_);
   // 事件循环必须在IO线程执行 如果不在代表发生错误，直接退出程序。
   assertInLoopThread();
@@ -112,20 +99,17 @@ void EventLoop::loop()
   quit_ = false;  // FIXME: what if someone calls quit() before loop() ?
   LOG_TRACE << "EventLoop " << this << " start looping";
 
-  while (!quit_)
-  {
+  while (!quit_) {
     activeChannels_.clear();
     // 调用poll返回活动的通道，有可能是唤醒返回的
     pollReturnTime_ = poller_->poll(kPollTimeMs, &activeChannels_);
     ++iteration_;
-    if (Logger::logLevel() <= Logger::TRACE)
-    {
+    if (Logger::logLevel() <= Logger::TRACE) {
       printActiveChannels();
     }
     // TODO sort channel by priority
     eventHandling_ = true;
-    for (Channel* channel : activeChannels_)
-    {
+    for (Channel* channel : activeChannels_) {
       currentActiveChannel_ = channel;
       currentActiveChannel_->handleEvent(pollReturnTime_);
     }
@@ -140,126 +124,102 @@ void EventLoop::loop()
 }
 
 // 该函数可以跨线程调用
-void EventLoop::quit()
-{
+void EventLoop::quit() {
   quit_ = true;
   // There is a chance that loop() just executes while(!quit_) and exits,
   // then EventLoop destructs, then we are accessing an invalid object.
   // Can be fixed using mutex_ in both places.
-  if (!isInLoopThread())
-  {
+  if (!isInLoopThread()) {
     wakeup();
   }
 }
 
 // 在I/O线程中执行某个回调函数，该函数可以跨线程调用
-void EventLoop::runInLoop(Functor cb)
-{
-  if (isInLoopThread())
-  {
+void EventLoop::runInLoop(Functor cb) {
+  if (isInLoopThread()) {
     // 如果是当前IO线程调用runInLoop，则同步调用cb
     cb();
-  }
-  else
-  {
+  } else {
     // 如果是其它线程调用runInLoop，则异步地将cb添加到队列
     queueInLoop(std::move(cb));
   }
 }
 
-void EventLoop::queueInLoop(Functor cb)
-{
+void EventLoop::queueInLoop(Functor cb) {
   {
-  std::lock_guard<std::mutex> lock(mutex_);
-  pendingFunctors_.push_back(std::move(cb));
+    std::lock_guard<std::mutex> lock(mutex_);
+    pendingFunctors_.push_back(std::move(cb));
   }
 
   // 调用queueInLoop的线程不是IO线程需要唤醒
   // 或者调用queueInLoop的线程是IO线程，并且此时正在调用pending functor，需要唤醒
   // 只有IO线程的事件回调中调用queueInLoop才不需要唤醒
-  if (!isInLoopThread() || callingPendingFunctors_)
-  {
+  if (!isInLoopThread() || callingPendingFunctors_) {
     wakeup();
   }
 }
 
-size_t EventLoop::queueSize() const
-{
+size_t EventLoop::queueSize() const {
   std::lock_guard<std::mutex> lock(mutex_);
   return pendingFunctors_.size();
 }
 
-TimerId EventLoop::runAt(Timestamp time, TimerCallback cb)
-{
+TimerId EventLoop::runAt(Timestamp time, TimerCallback cb) {
   return timerQueue_->addTimer(std::move(cb), time, 0.0);
 }
 
-TimerId EventLoop::runAfter(double delay, TimerCallback cb)
-{
+TimerId EventLoop::runAfter(double delay, TimerCallback cb) {
   Timestamp time(addTime(Timestamp::now(), delay));
   return runAt(time, std::move(cb));
 }
 
-TimerId EventLoop::runEvery(double interval, TimerCallback cb)
-{
+TimerId EventLoop::runEvery(double interval, TimerCallback cb) {
   Timestamp time(addTime(Timestamp::now(), interval));
   return timerQueue_->addTimer(std::move(cb), time, interval);
 }
 
-void EventLoop::cancel(TimerId timerId)
-{
-  return timerQueue_->cancel(timerId);
-}
+void EventLoop::cancel(TimerId timerId) { return timerQueue_->cancel(timerId); }
 
-void EventLoop::updateChannel(Channel* channel)
-{
+void EventLoop::updateChannel(Channel* channel) {
   assert(channel->ownerLoop() == this);
   assertInLoopThread();
   poller_->updateChannel(channel);
 }
 
-void EventLoop::removeChannel(Channel* channel)
-{
+void EventLoop::removeChannel(Channel* channel) {
   assert(channel->ownerLoop() == this);
   assertInLoopThread();
-  if (eventHandling_)
-  {
+  if (eventHandling_) {
     assert(currentActiveChannel_ == channel ||
         std::find(activeChannels_.begin(), activeChannels_.end(), channel) == activeChannels_.end());
   }
   poller_->removeChannel(channel);
 }
 
-bool EventLoop::hasChannel(Channel* channel)
-{
+bool EventLoop::hasChannel(Channel* channel) {
   assert(channel->ownerLoop() == this);
   assertInLoopThread();
   return poller_->hasChannel(channel);
 }
 
-void EventLoop::abortNotInLoopThread()
-{
+void EventLoop::abortNotInLoopThread() {
   LOG_FATAL << "EventLoop::abortNotInLoopThread - EventLoop " << this
             << " was created in threadId_ = " << threadId_
-            << ", current thread id = " <<  CurrentThread::tid();
+            << ", current thread id = " << CurrentThread::tid();
 }
 
-void EventLoop::wakeup()
-{
+void EventLoop::wakeup() {
   uint64_t one = 1;
   ssize_t n = sockets::write(wakeupFd_, &one, sizeof one);
-  if (n != sizeof one)
-  {
+  if (n != sizeof one) {
     LOG_ERROR << "EventLoop::wakeup() writes " << n << " bytes instead of 8";
   }
 }
 
-void EventLoop::handleRead()
-{
+void EventLoop::handleRead() {
   uint64_t one = 1;
   ssize_t n = sockets::read(wakeupFd_, &one, sizeof one);
-  if (n != sizeof one)
-  {
+  if (n != sizeof one) {
     LOG_ERROR << "EventLoop::handleRead() reads " << n << " bytes instead of 8";
   }
 }
@@ -269,35 +229,30 @@ void EventLoop::handleRead()
  * 而是把回调列表swap到functors中，
  * 这样一方面减小了临界区的长度（意味着不会阻塞其它线程的queueInLoop()），
  * 另一方面，也避免了死锁（因为Functor可能再次调用queueInLoop()）
- * 
+ *
  * 由于doPendingFunctors()调用的Functor可能再次调用queueInLoop(cb)，
  * 这时，queueInLoop()就必须wakeup()，否则新增的cb可能就不能及时调用了
- * 
+ *
  * muduo没有反复执行doPendingFunctors()直到pendingFunctors为空，
  * 这是有意的，否则IO线程可能陷入死循环，无法处理IO事件。
  */
-void EventLoop::doPendingFunctors()
-{
+void EventLoop::doPendingFunctors() {
   std::vector<Functor> functors;
   callingPendingFunctors_ = true;
 
   {
-  std::lock_guard<std::mutex> lock(mutex_);
-  functors.swap(pendingFunctors_);
+    std::lock_guard<std::mutex> lock(mutex_);
+    functors.swap(pendingFunctors_);
   }
 
-  for (const Functor& functor : functors)
-  {
+  for (const Functor& functor : functors) {
     functor();
   }
   callingPendingFunctors_ = false;
 }
 
-void EventLoop::printActiveChannels() const
-{
-  for (const Channel* channel : activeChannels_)
-  {
+void EventLoop::printActiveChannels() const {
+  for (const Channel* channel : activeChannels_) {
     LOG_TRACE << "{" << channel->reventsToString() << "} ";
   }
 }
-
