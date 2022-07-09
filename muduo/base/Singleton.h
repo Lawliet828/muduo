cb53a4501cbbca7d3e2de2fcb4f0b33807941861
lawliet <jiecfd@gmail.com>
2022-07-09 14:22:15 +0800

    单列

--- muduo/base/Singleton.h
+++ muduo/base/Singleton.h
@@ -6,57 +6,52 @@
 #ifndef MUDUO_BASE_SINGLETON_H
 #define MUDUO_BASE_SINGLETON_H
 
+#include <assert.h>
+#include <stdlib.h>  // atexit
+
+#include <mutex>  // for once_flag call_once
+
 #include "muduo/base/noncopyable.h"
 
-#include <assert.h>
-#include <mutex> // for once_flag call_once
-#include <pthread.h>
-#include <stdlib.h> // atexit
+namespace muduo {
 
-namespace muduo
-{
-
-namespace detail
-{
+namespace detail {
 // This doesn't detect inherited member functions!
 // http://stackoverflow.com/questions/1966362/sfinae-to-check-for-inherited-member-functions
-template<typename T>
-struct has_no_destroy
-{
-  template <typename C> static char test(decltype(&C::no_destroy));
-  template <typename C> static int32_t test(...);
+template <typename T>
+struct has_no_destroy {
+  template <typename C>
+  static char test(decltype(&C::no_destroy));
+  template <typename C>
+  static int32_t test(...);
   const static bool value = sizeof(test<T>(0)) == 1;
 };
 }  // namespace detail
 
-template<typename T>
-class Singleton : noncopyable
-{
+template <typename T>
+class Singleton : noncopyable {
  public:
   Singleton() = delete;
   ~Singleton() = delete;
 
-  static T& instance()
-  {
+  static T& instance() {
     std::call_once(once_, &Singleton::init);
     assert(value_ != NULL);
     return *value_;
   }
 
  private:
-  static void init()
-  {
+  static void init() {
     value_ = new T();
-    if (!detail::has_no_destroy<T>::value)
-    {
+    if (!detail::has_no_destroy<T>::value) {
       ::atexit(destroy);
     }
   }
 
-  static void destroy()
-  {
+  static void destroy() {
     typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
-    T_must_be_complete_type dummy; (void) dummy;
+    T_must_be_complete_type dummy;
+    (void)dummy;
 
     delete value_;
     value_ = NULL;
@@ -64,13 +59,13 @@ class Singleton : noncopyable
 
  private:
   static std::once_flag once_;
-  static T*             value_;
+  static T* value_;
 };
 
-template<typename T>
+template <typename T>
 std::once_flag Singleton<T>::once_;
 
-template<typename T>
+template <typename T>
 T* Singleton<T>::value_ = NULL;
 
 }  // namespace muduo
--- muduo/base/tests/Singleton_test.cc
+++ muduo/base/tests/Singleton_test.cc
@@ -1,65 +1,60 @@
 #include "muduo/base/Singleton.h"
-#include "muduo/base/CurrentThread.h"
-#include "muduo/base/Thread.h"
 
 #include <stdio.h>
 
-class Test : muduo::noncopyable
-{
+#include <thread>
+
+#include "muduo/base/CurrentThread.h"
+
+class Test : muduo::noncopyable {
  public:
-  Test()
-  {
+  Test() {
     printf("tid=%d, constructing %p\n", muduo::CurrentThread::tid(), this);
   }
 
-  ~Test()
-  {
-    printf("tid=%d, destructing %p %s\n", muduo::CurrentThread::tid(), this, name_.c_str());
+  ~Test() {
+    printf("tid=%d, destructing %p %s\n", muduo::CurrentThread::tid(), this,
+           name_.c_str());
   }
 
-  const muduo::string& name() const { return name_; }
-  void setName(const muduo::string& n) { name_ = n; }
+  const std::string& name() const { return name_; }
+  void setName(const std::string& n) { name_ = n; }
 
  private:
-  muduo::string name_;
+  std::string name_;
 };
 
-class TestNoDestroy : muduo::noncopyable
-{
+class TestNoDestroy : muduo::noncopyable {
  public:
   // Tag member for Singleton<T>
   void no_destroy();
 
-  TestNoDestroy()
-  {
-    printf("tid=%d, constructing TestNoDestroy %p\n", muduo::CurrentThread::tid(), this);
+  TestNoDestroy() {
+    printf("tid=%d, constructing TestNoDestroy %p\n",
+           muduo::CurrentThread::tid(), this);
   }
 
-  ~TestNoDestroy()
-  {
-    printf("tid=%d, destructing TestNoDestroy %p\n", muduo::CurrentThread::tid(), this);
+  ~TestNoDestroy() {
+    printf("tid=%d, destructing TestNoDestroy %p\n",
+           muduo::CurrentThread::tid(), this);
   }
 };
 
-void threadFunc()
-{
-  printf("tid=%d, %p name=%s\n",
-         muduo::CurrentThread::tid(),
+void threadFunc() {
+  printf("tid=%d, %p name=%s\n", muduo::CurrentThread::tid(),
          &muduo::Singleton<Test>::instance(),
          muduo::Singleton<Test>::instance().name().c_str());
   muduo::Singleton<Test>::instance().setName("only one, changed");
 }
 
-int main()
-{
+int main() {
   muduo::Singleton<Test>::instance().setName("only one");
-  muduo::Thread t1(threadFunc);
-  t1.start();
+  std::thread t1(threadFunc);
   t1.join();
-  printf("tid=%d, %p name=%s\n",
-         muduo::CurrentThread::tid(),
+  printf("tid=%d, %p name=%s\n", muduo::CurrentThread::tid(),
          &muduo::Singleton<Test>::instance(),
          muduo::Singleton<Test>::instance().name().c_str());
   muduo::Singleton<TestNoDestroy>::instance();
-  printf("with valgrind, you should see %zd-byte memory leak.\n", sizeof(TestNoDestroy));
+  printf("with valgrind, you should see %zd-byte memory leak.\n",
+         sizeof(TestNoDestroy));
 }
