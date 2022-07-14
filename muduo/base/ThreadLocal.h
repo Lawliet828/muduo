14a3baf48a7597d5a133ef397c90d821ce60be44
lawliet <jiecfd@gmail.com>
2022-07-14 10:40:35 +0800

    删除CHECK_PTHREAD_RETURN_VALUE

--- muduo/base/ThreadLocal.h
+++ muduo/base/ThreadLocal.h
@@ -10,28 +10,8 @@
 
 #include "muduo/base/noncopyable.h"
 
-#ifdef CHECK_PTHREAD_RETURN_VALUE
-
-#ifdef NDEBUG
-__BEGIN_DECLS
-extern void __assert_perror_fail (int errnum,
-                                  const char *file,
-                                  unsigned int line,
-                                  const char *function)
-    noexcept __attribute__ ((__noreturn__));
-__END_DECLS
-#endif
-
-#define MCHECK(ret) ({ __typeof__ (ret) errnum = (ret);         \
-                       if (UNLIKELY(errnum != 0))    \
-                         __assert_perror_fail (errnum, __FILE__, __LINE__, __func__);})
-
-#else  // CHECK_PTHREAD_RETURN_VALUE
-
 #define MCHECK(ret) ({ __typeof__ (ret) errnum = (ret);         \
                        assert(errnum == 0); (void) errnum;})
-
-#endif // CHECK_PTHREAD_RETURN_VALUE
 
 namespace muduo {
 
